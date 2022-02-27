9ac5543c70ae0d2bb738f1b58c0d49b1ae13c463
Eric Veilleux <virx@virxcase.dev>
2022-02-26 19:30:06 -0500

    Use RLBot's python

--- develop_rlru.sh
+++ develop_rlru.sh
@@ -1,4 +1,5 @@
 set +v
 cargo fmt
 maturin build --release -i "/usr/bin/python3.7"
-python3.7 -m pip install target/wheels/VirxERLU_RLib-*.*.*-cp37-*.whl --force-reinstall
+source ~/.RLBotGUI/env/bin/activate
+pip install target/wheels/VirxERLU_RLib-*.*.*-cp37-*.whl --force-reinstall
--- test_rlru.sh
+++ test_rlru.sh
@@ -1,2 +1,3 @@
 set +v
+source ~/.RLBotGUI/env/bin/activate
 python3.7 pytest.py
