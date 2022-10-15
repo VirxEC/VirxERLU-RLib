2f78b90bf053a55b774be74ec5b0ef731dd8a72f
VirxEC <virx@virxcase.dev>
2022-10-15 17:01:05 -0400

    Fix publish_rlru.bat

--- publish_rlru.bat
+++ publish_rlru.bat
@@ -1,4 +1,4 @@
 @echo off
 "%localappdata%\RLBotGUIX\Python37\python.exe" -m pip install -U maturin twine
-maturin publish --release -i "%localappdata%\RLBotGUIX\Python37\python.exe"
+maturin publish -i "%localappdata%\RLBotGUIX\Python37\python.exe"
 pause
